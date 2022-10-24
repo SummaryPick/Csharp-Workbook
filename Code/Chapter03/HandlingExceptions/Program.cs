9642cff62a50271fedc5b455115d37f01aa0bb45
DESKTOP-HKMFP7A\Zacha <zachary.ryan.mcarthur@gmail.com>
2022-10-24 15:59:10 -0500

    Ch3 cont

---
+++ Code/Chapter03/HandlingExceptions/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace handlingexceptions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Before parsing");
+            Console.Write("What is your age?");
+            string input = Console.ReadLine();
+
+            try
+            {
+                int age = int.Parse(input);
+                Console.WriteLine($"You are {age} years old.");
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("The age you entered is not a valid number format.");
+            }
+            catch (OverflowException)
+            {
+                Console.WriteLine("Your age is a valid number but is either too big or too small.");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"{ex.GetType()} says {ex.Message}");
+            }
+            Console.WriteLine("After Parsing.");
+        }
+    }
+}
