7d5d180d02b0f889f759130ff7a072073a1eb4e8
DESKTOP-HKMFP7A\Zacha <zachary.ryan.mcarthur@gmail.com>
2022-12-07 01:03:29 -0600

    Chapter 4 and start of 5

---
+++ Code/Chapter04/Instrumenting/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using Microsoft.Extensions.Configuration;
+
+namespace Instrumenting;
+class Program
+{
+    static void Main(string[] args)
+    {
+        //Write to a text file in the project folder
+        Trace.Listeners.Add(new TextWriterTraceListener(File.CreateText("log.txt")));
+        Trace.AutoFlush = true;
+
+        Debug.WriteLine("Debug is watching.");
+        Trace.WriteLine("Trace is watching.");
+
+        var builder = new ConfigurationBuilder()
+            .SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile("appsettings.json", 
+            optional: true, reloadOnChange: true);
+
+        IConfigurationRoot configuration = builder.Build();
+
+        var ts = new TraceSwitch(
+            displayName: "PacktSwtich",
+            description: "This switch is set via a JSON config.");
+        
+        configuration.GetSection("PacktSwitch").Bind(ts);
+
+        Trace.WriteLineIf(ts.TraceError, "TraceError");
+        Trace.WriteLineIf(ts.TraceWarning, "TraceWarning");
+        Trace.WriteLineIf(ts.TraceInfo, "Trace Information");
+        Trace.WriteLineIf(ts.TraceVerbose, "TraceVerbose");
+    }
+}
---
+++ Code/Chapter05/PeopleApp/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using static System.Console;
+using Packt.Shared;
+
+namespace PeopleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var bob = new Person();
+            bob.Name = "Bob Smith";
+            bob.DateOfBirth = new DateTime(1965, 12, 22);
+            bob.FavoriteAncientWonder = WondersOfTheAncientWorld.StatueOfZuesAtOlympia;
+
+            WriteLine(
+                format: "{0} was born on {1:dddd, MMMM yyyy}",
+                arg0: bob.Name,
+                arg1: bob.DateOfBirth
+            );
+
+            WriteLine(
+                format: "{0}'s favorite wonder is {1}. Its integer is {2}.",
+                arg0: bob.Name,
+                arg1: bob.FavoriteAncientWonder,
+                arg2: (int)bob.FavoriteAncientWonder
+            );
+
+            var alice = new Person 
+            {
+                Name = "Alice Jones",
+                DateOfBirth = new DateTime(1994, 9, 7)
+            };
+
+            WriteLine(
+                format: "{0} was born on {1:dd, MMM yy}",
+                arg0: alice.Name,
+                arg1: alice.DateOfBirth
+            );
+        }
+    }
+}
