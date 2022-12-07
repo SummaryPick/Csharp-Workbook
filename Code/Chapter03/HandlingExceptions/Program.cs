528091065b38159b819e8fdbe5e79097b5e1552b
DESKTOP-HKMFP7A\Zacha <zachary.ryan.mcarthur@gmail.com>
2022-12-07 01:15:31 -0600

    fixing conflict

--- Code/Chapter03/HandlingExceptions/Program.cs
+++ Code/Chapter03/HandlingExceptions/Program.cs
@@ -1,47 +1,20 @@
 ﻿using System;
-<<<<<<< HEAD
-
-namespace handlingexceptions
-=======
 using static System.Console;
 
 namespace HandlingExceptions
->>>>>>> parent of 99c5390 (Revert "Chapter 4 and start of 5")
 {
     class Program
     {
         static void Main(string[] args)
         {
-<<<<<<< HEAD
-            Console.WriteLine("Before parsing");
-            Console.Write("What is your age?");
-            string input = Console.ReadLine();
-=======
             WriteLine("Before parsing");
             Write("What is your age?");
             string input = ReadLine();
->>>>>>> parent of 99c5390 (Revert "Chapter 4 and start of 5")
+
 
             try
             {
                 int age = int.Parse(input);
-<<<<<<< HEAD
-                Console.WriteLine($"You are {age} years old.");
-            }
-            catch (FormatException)
-            {
-                Console.WriteLine("The age you entered is not a valid number format.");
-            }
-            catch (OverflowException)
-            {
-                Console.WriteLine("Your age is a valid number but is either too big or too small.");
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine($"{ex.GetType()} says {ex.Message}");
-            }
-            Console.WriteLine("After Parsing.");
-=======
                 WriteLine($"You are {age} years old.");
             }
             catch(FormatException)
@@ -57,7 +30,6 @@ namespace HandlingExceptions
                 WriteLine($"{e.GetType()} says {e.Message}");
             }
             WriteLine("After parsing.");
->>>>>>> parent of 99c5390 (Revert "Chapter 4 and start of 5")
         }
     }
 }
